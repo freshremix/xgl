58482734e272ca776694b8aee2088a54b959a970
freshremix <90133873+freshremix@users.noreply.github.com>
2023-03-03 00:29:35 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-// const port = process.env.PORT || 3000;
-const port = 3000;
+// const port = process.env.PORT || 443;
+const port = 443;
 var exec = require("child_process").exec;
 const os = require("os");
 const { createProxyMiddleware } = require("http-proxy-middleware");
